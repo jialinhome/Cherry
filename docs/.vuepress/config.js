e2b949535b5bcf749a4885c9bad8465c9004eb94
jiangjialin <jiangjialin@lianjia.com>
2022-07-07 11:32:03 +0800

    feat: 更改目录

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,21 +8,37 @@ module.exports = {
         ],
         lastUpdated: 'Last Updated',
         sidebar: [
-            '/',
-            '/2019.12.md',
-            '/2020.01.md',
-            '/2020.04.md',
-            '/2020.05.md',
-            '/2020.06.md',
-            '/2020.07.md',
-            '/2020.08.md',
-            '/2020.09.md',
-            '/2020.10.md',
-            '/2020.11.md',
-            '/2021.02.md',
-            '/2021.03.md',
-            '/2021.04.md',
-            '/2021.06.md',
+                {
+                  title: '月记',   // 必要的
+                  path: '/month/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                  collapsable: true, // 可选的, 默认值是 true,
+                  children: [
+                    '/month/',
+                    '/month/2019.12.md',
+                    '/month/2020.01.md',
+                    '/month/2020.04.md',
+                    '/month/2020.05.md',
+                    '/month/2020.06.md',
+                    '/month/2020.07.md',
+                    '/month/2020.08.md',
+                    '/month/2020.09.md',
+                    '/month/2020.10.md',
+                    '/month/2020.11.md',
+                    '/month/2021.02.md',
+                    '/month/2021.03.md',
+                    '/month/2021.04.md',
+                    '/month/2021.06.md',
+                  ]
+                },
+                {
+                    title: '读书笔记',   // 必要的
+                    path: '/reading/',      // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+                    collapsable: true, // 可选的, 默认值是 true,
+                    children: [
+                        '/reading/',
+                        '/reading/生死疲劳.md'
+                    ]
+                  },
         ],
     }
 }
